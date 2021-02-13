6c544005a7526a6d19042061fa4a4c6cd5a3d9a1
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-02-13 14:04:16 +0100

    Finale Version

--- main.go
+++ main.go
@@ -18,17 +18,44 @@ import (
 
 func main() {
 
+	// Zur Datenbank verbinden
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		DB:       0,
 		Password: "",
 	})
 
-	result, err := rdb.HDel("client:kwiedor", "age").Result()
+	// Einen string erstellen
+	err := rdb.Set("client:kwiedor", "Jonas Kwiedor", 0).Err()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(result)
+	// String auslesen
+	client, err := rdb.Get("client:kwiedor").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Client ausgeben
+	fmt.Println(client)
+
+	// Hash erstellen
+	fields := make(map[string]interface{})
+
+	fields["name"] = "Jonas Kwiedor"
+	fields["age"] = 23
+	fields["job"] = "Developer"
+
+	err = rdb.HMSet("client:kwiedor2", fields).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Feld aus hash entfernen
+	err = rdb.HDel("client:kwiedor", "age").Err()
+	if err != nil {
+		fmt.Println(err)
+	}
 
 }
